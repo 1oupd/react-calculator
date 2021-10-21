7cb9758dcbf346da51134dde3f772d1b9d93ae32
danny vincent <louisa.duncan44@outlook.com>
2021-10-21 14:34:44 +0100

    Added styling, buttons, etc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div>
 
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++){
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
+
+  return (
+    <div className="calculator">
+      <div className="display">
+       <span>⠀0⠀</span>
+      </div>
+      <div className="operators">
+        <button>/</button>
+        <button>*</button>
+        <button>+</button>
+        <button>-</button>
+        <button>del</button>
+      </div>
+      <div className="digits">
+        {createDigits()}
+      <button>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
     </div>
+  
+
   );
 }
 
